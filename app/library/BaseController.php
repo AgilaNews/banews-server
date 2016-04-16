b3f853f842f407e183a6cf876681756441bf8b84
zhangguanxing <fagging@qq.com>
2016-04-16 17:38:50 +0800

    add index status code handle

--- app/library/BaseController.php
+++ app/library/BaseController.php
@@ -14,10 +14,12 @@ class BaseController extends Controller{
         if ($this->cookies->has('session')) {
             $this->sessionId = $this->cookies->get('session')->getValue();
         } else {
+            /*
             $cookies = new Cookies();
             $cookies->useEncryption(false);
             $cookies->set("session", UserSession::genSessionId($this->deviceToken));
             $this->di->set("cookies", $cookies);
+            */
         }
         $this->logger = $this->di->get('logger');
         $this->logger->begin();
