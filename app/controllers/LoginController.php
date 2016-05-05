155505920dea8f0740f6cf140b7538699b311f2d
zhangguanxing <fagging@qq.com>
2016-05-05 15:02:43 +0800

    first usable version

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends BaseController {
         $source_name = $this->get_or_fail($req, "source", "string");
         $source = $this->get_or_fail(User::SOURCE_MAP, $source_name, "int");
         $user = User::getBySourceAndId($source, $uid);
-        var_dump($user);
 
         if (!$user) {
             $user = new User();
