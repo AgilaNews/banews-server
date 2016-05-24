afeca9773e3a2f55269962c499e9a1de10de4f6b
zhangguanxing <zgxcassar@gmaol.com>
2016-05-24 18:21:47 +0800

    add control

--- 1/app/policies/BaseListPolicy.php
+++ 1/app/policies/BaseListPolicy.php
@@ -30,7 +30,7 @@ abstract class BaseListPolicy {
 
         foreach ($ready_news_list as $ready_news=>$weight) {
             if (!in_array($ready_news, $sent)) {
-                $valid_news_list = array("id" => $ready_news,
+                $valid_news_list []= array("id" => $ready_news,
                                          "ptime" => $weight);
             }
         }
