a495f886838b5530630c92f77c30c7d279128a95
zhangguanxing <zgxcassar@gmaol.com>
2016-05-31 11:46:45 +0800

    add all

--- entry/public/index.php
+++ entry/public/index.php
@@ -14,7 +14,7 @@ define('H5_SERVER_NAME', "m.agilanews.today");
 define('ANDROID_VERSION_CODE', 2);
 define('MIN_VERSION', "1.0.0"); //TODO change this to a configuration center
 define('NEW_VERSION', "1.0.0");
-define('UPDATE_URL', "http://demoupdate.googleplay.com");
+define('UPDATE_URL', "https://play.google.com/apps/testing/com.upeninsula.banews");
 
 use Phalcon\Config;
 use Phalcon\Mvc\Application;
