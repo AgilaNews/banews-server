fb44c14fb1dd1a409679527c48a306cd44522472
zhangguanxing <zgxcassar@gmaol.com>
2016-06-07 20:17:34 +0800

    add all

--- entry/public/index.php
+++ entry/public/index.php
@@ -3,13 +3,9 @@
 error_reporting(E_ALL);
 
 define('APP_PATH', realpath('..') . '/');
-define('SERVER_NAME', "api.agilanews.today");
 define('ERR_KEY', 40001);
 define('ERR_CLIENT_VERSION_NOT_FOUND', 40011);
 define('ERR_INTERNAL_DB', 50002);
-define('LOG_SERVER_NAME', 'log.agilanews.today');
-define('MON_SERVER_NAME', 'mon.agilanews.today');
-define('H5_SERVER_NAME', "m.agilanews.today");
 
 define('ANDROID_VERSION_CODE', 5);
 define('MIN_VERSION', "v1.0.0"); //TODO change this to a configuration center
@@ -24,11 +20,24 @@ try {
 
     if ($env == "rd") {
         require APP_PATH . "app/config/config.php.rd";
+        define('SERVER_NAME', "api.agilanews.today");
+        define('LOG_SERVER_NAME', 'log.agilanews.today');
+        define('MON_SERVER_NAME', 'mon.agilanews.today');
+        define('H5_SERVER_NAME', "m.agilanews.today");
         define('BA_DEBUG', true);
     } if ($env == "sandbox") {
         require APP_PATH . "app/config/config.php.sandbox";
+        define('SERVER_NAME', "api.agilanews.info");
+        define('LOG_SERVER_NAME', 'log.agilanews.info');
+        define('MON_SERVER_NAME', 'mon.agilanews.info');
+        define('H5_SERVER_NAME', "m.agilanews.info");
         define('BA_DEBUG', true);
     } else {
+        define('SERVER_NAME', "api.agilanews.today");
+        define('LOG_SERVER_NAME', 'log.agilanews.today');
+        define('MON_SERVER_NAME', 'mon.agilanews.today');
+        define('H5_SERVER_NAME', "m.agilanews.today");
+
         require APP_PATH . "app/config/config.php";
     }
     $config = new Config($settings);
