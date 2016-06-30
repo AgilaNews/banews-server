8b18a7217cfb8a3a39c5979de9f443cc563bb2e8
zhangguanxing <zgxcassar@gmaol.com>
2016-06-30 18:49:17 +0800

    reconstruct the code, add render and selectors

--- 1/app/selectors/Selector10012.php
+++ 1/app/selectors/Selector10012.php
@@ -1,6 +1,4 @@
 <?php
-define("MIN_IMG_SENT_COUNT", 10);
-define("MAX_IMG_SEND_COUNT", 12);
 class Selector10012 extends Selector10011 {
     public function __construct($channel_id, $di) {
         parent::__construct($channel_id, $di);
