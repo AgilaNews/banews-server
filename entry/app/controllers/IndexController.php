85b6f633dfb705d2d26707c66dd9972a44d85cec
zhangguanxing <zgxcassar@gmaol.com>
2016-07-13 22:41:39 +0800

    add cache

--- entry/app/controllers/IndexController.php
+++ entry/app/controllers/IndexController.php
@@ -39,17 +39,17 @@ class IndexController extends BaseController {
 
         if (version_compare(substr($client_version, 1), "1.1.2", ">=")) {
             $interfaces = array(
-                    "home" => sprintf($this->config->entries->homes, $vm->server_version),
-                    "mon" => sprintf($this->config->entries->mons, $vm->server_version),
-                    "log" => sprintf($this->config->entries->logs, $vm->server_version),
+                    "home" => sprintf($this->config->entries->home, $vm->server_version),
+                    "mon" => sprintf($this->config->entries->mon, $vm->server_version),
+                    "log" => sprintf($this->config->entries->log, $vm->server_version),
                     "referrer" => $this->config->entries->referrers
                     );
         } else {
             $interfaces = array(
-                    "home" => sprintf($this->config->entries->home, $vm->server_version),
-                    "mon" => sprintf($this->config->entries->mon, $vm->server_version),
-                    "log" => sprintf($this->config->entries->log, $vm->server_version),
-                    "referrer" => $this->config->entries->referrer);
+                    "home" => sprintf($this->config->entries->homes, $vm->server_version),
+                    "mon" => sprintf($this->config->entries->mons, $vm->server_version),
+                    "log" => sprintf($this->config->entries->logs, $vm->server_version),
+                    "referrer" => $this->config->entries->referrers);
         }
 
         $ret = array(
