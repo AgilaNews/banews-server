125939ff4b5be8b257f9eba9ebd5a7da584377e8
zhangguanxing <zgxcassar@gmaol.com>
2016-08-18 12:18:30 +0800

    add all

--- entry/app/controllers/IndexController.php
+++ entry/app/controllers/IndexController.php
@@ -37,21 +37,14 @@ class IndexController extends BaseController {
                                     "client version not supported");
         }
 
-        //if (version_compare(substr($client_version, 1), "1.1.2", ">=")) {
+        
         $interfaces = array(
                "home" => sprintf($this->config->entries->home, $vm->server_version),
                "mon" => sprintf($this->config->entries->mon, $vm->server_version),
                "log" => sprintf($this->config->entries->log, $vm->server_version),
                "referrer" => $this->config->entries->referrer
                );
-       // } else {
-       //     $interfaces = array(
-       //             "home" => sprintf($this->config->entries->homes, $vm->server_version),
-       //             "mon" => sprintf($this->config->entries->mons, $vm->server_version),
-       //             "log" => sprintf($this->config->entries->logs, $vm->server_version),
-       //             "referrer" => $this->config->entries->referrers);
-       // }
-
+        
         $ret = array(
                 "interfaces" => $interfaces,
                 "updates" => array(
