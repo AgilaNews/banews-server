7f74d1ea46362fa5604d1c4f234a9fbdfb953b0e
limeng <lemon.li.hust@gmail.com>
2016-08-25 19:07:05 +0800

    add new strategy file

---
+++ library/policies/EsRelatedRecPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+use Elasticsearch\ClientBuilder;
+
+class EsRelatedRecPolicy extends BaseRecommendPolicy {
+    public function __construct($di) {
+        parent::__construct($di);
+        $clientBuilder = ClientBuilder::create();
+        $hosts = ['http://10.8.18.130:9200'];
+        $clientBuilder->setHosts($hosts);
+        $this->esClient = $clientBuilder->build();
+    }
+
+    protected function moreLikeThis($myself) {
+        $searchParams = array(
+            'index' => 'banews-article',
+            'type' => 'article',
+            'body' => array(
+                'query' => array(
+                    'more_like_this' => array(
+                        'fields' => array('title', 'plain_text'),
+                        'like' => array(
+                            '_index' => 'banews-article',
+                            '_type' => 'article',
+                            '_id' => $myself,
+                        ),
+                        'min_term_freq' => 1,
+                        'min_doc_freq' => 1,
+                    ),
+                ),
+            ),
+        );
+        $response = $this->esClient->search($searchParams);
+        print_r($response);
+    }
+
+    public function sampling($channel_id, $device_id, $user_id, $myself, 
+        $pn=3, $day_till_now=7, array $options=null) {
+    }
+}
+
