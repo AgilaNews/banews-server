908e26de2fb20dd3b249854cab1768cde26d3f52
limeng <lemon.li.hust@gmail.com>
2016-08-26 14:10:52 +0800

    debug

--- library/policies/EsRelatedRecPolicy.php
+++ library/policies/EsRelatedRecPolicy.php
@@ -1,6 +1,7 @@
 <?php
 
 use Elasticsearch\ClientBuilder;
+use Phalcon\DI;
 
 class EsRelatedRecPolicy extends BaseRecommendPolicy {
     public function __construct($di) {
@@ -9,6 +10,7 @@ class EsRelatedRecPolicy extends BaseRecommendPolicy {
         $hosts = ['http://10.8.18.130:9200'];
         $clientBuilder->setHosts($hosts);
         $this->esClient = $clientBuilder->build();
+        $this->logger = DI::getDefault()->get('logger');
     }
 
     protected function moreLikeThis($myself) {
@@ -24,15 +26,23 @@ class EsRelatedRecPolicy extends BaseRecommendPolicy {
                             '_type' => 'article',
                             '_id' => $myself,
                         ),
+                        'max_query_terms' => 30,
                         'min_term_freq' => 1,
                         'min_doc_freq' => 1,
                     ),
                 ),
             ),
         );
-        $response = $this->esClient->search($searchParams);
-        var_dump($response);
-        exit(0);
+        try {
+            $relatedNews = $this->esClient->search($searchParams);
+            foreach($relatedNews as $curNews) {
+                var_dump($relatedNews);
+            }
+            exit(0);
+        } catch(\Exception $e) {
+            $this->logger.error(sprintf("[file:%s][line:%s][message:%s][code:%s]", 
+                $e->getFile(), $e->getLine(), $e->getMessage(), $e->getCode()));
+        }
     }
 
     public function sampling($channel_id, $device_id, $user_id, $myself, 
