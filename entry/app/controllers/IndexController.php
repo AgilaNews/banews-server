4881516205981c33bcc8b0d0e2f485d45541667a
zhangguanxing <zgxcassar@gmaol.com>
2016-08-31 19:10:51 +0800

    add version cehck

--- entry/app/controllers/IndexController.php
+++ entry/app/controllers/IndexController.php
@@ -28,84 +28,19 @@ class IndexController extends BaseController {
         if (!$this->client_version) {
             throw new HttpException(ERR_CLIENT_VERSION_NOT_FOUND, 'client version not found');    
         }
-       
-        $ret = Version::getAllUseable();
-        if (!$ret) {
-            throw new HttpException(ERR_INTERNAL_DB, "internal error");
+
+        $update_info = Version::getUpdateInfo($this->client_version, $this->os, $this->build);
+        if (!$update_info) {
+            throw new HttpException(ERR_INTERNAL_DB,
+                                    "internal error");
         }
-
-        $models = array();
-
-        foreach ($ret as $model) {
-            $models []= $model;
+        if (!$update_info["models"]["cur"]) {
+            throw new HttpException(ERR_CLIENT_VERSION_NOT_FOUND, 'client version not supported');    
         }
+        
+        $cur_model = $update_info["models"]["cur"];
+        unset($update_info["models"]);
 
-        usort($models, function($a, $b) {
-            return version_compare($a->client_version, $b->client_version);
-        });
-
-        if (count($models) == 0) {
-            throw new HttpException(ERR_INTERNAL_DB, "internal error");
-        }
-
-        $cur_model = $min_model = $new_model = null;
-
-        /*
-           this section is relative tricky
-           we have a sequence of version, some version is published for ios while others published for android
-           some version number may skip serveral version 
-            | for example, android develops v1.1.0 v1.1.1 v1.1.2, v1.1.1 is bug fix version, so v1.1.1 is not useful for ios
-            | so ios do not have v1.1.1 version
-
-           We defined four states by two bits to identify this situation
-           0 not published for all
-           1 only published for android
-           2 only published for ios
-           1 | 2 published for all platforms
-
-           
-           note: android has two main build package, main app is 1 called `Agila`, accessory is 2 called `Agila News`, fuck
-
-           so we make sure that versions is sorted above, then just iterate from oldest version to newest version
-           we can get three version numbers:
-            1. min_version, minmal usable version, the first one we check is usable is certain platform
-            2. new_version, last usable version, we keep track of usable version util nothing more saw
-            3. cur_version, client version number set by client, some actions taken by server is based on client version
-        */
-        for ($i = 0; $i < count($models); $i++) {
-            $model = $models[$i];
-            if ($this->os == "ios") {
-                if ($model->status & IOS_PUBLISHED) {
-                    $new_model = $model;
-
-                    if (!$min_model) {
-                        $min_model = $model;
-                    }
-                    if ($model->client_version == $this->client_version) {
-                        $cur_model = $model;
-                    }
-                }
-            }
-
-            if ($this->os == "android") {
-                if ($model->status & ANDROID_PUBLISHED && $model->build_type == $this->build) {
-                    $new_model = $model;
-
-                    if (!$min_model) {
-                        $min_model = $model;
-                    }
-                    if ($model->client_version == $this->client_version) {
-                        $cur_model = $model;
-                    }
-                }
-            }
-        }
-
-        if (!$cur_model) {
-            throw new HttpException(ERR_CLIENT_VERSION_NOT_FOUND,
-                                    "client version not supported");
-        }
-     
         $ret = array (
             "interfaces" => array(
                 "home" => sprintf($this->config->entries->home, $cur_model->server_version),
@@ -113,19 +48,9 @@ class IndexController extends BaseController {
                 "log" => sprintf($this->config->entries->log, $cur_model->server_version),
                 "referrer" => $this->config->entries->referrer,
             ),
-            "updates" => array(
-                "min_version" => "v" . $min_model->client_version,
-                "new_version" => "v" . $new_model->client_version,
-            ),
+            "updates" => $update_info,
         );
         
-        if ($this->os == "ios") {
-            $ret["updates"]["update_url"] = $new_model->ios_update_url;
-        } else {
-            $ret["updates"]["update_url"] = $new_model->update_url;
-            $ret["updates"]["avc"] = $new_model->android_version_code;
-        }
-
         if ($cur_model->server_version == 1) {
             $ret["categories"] = array();
 
