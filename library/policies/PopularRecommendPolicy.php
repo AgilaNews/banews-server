e2819fc25ebdb27750b191c947bed1984237b1a9
Zhang Yaxuan <zhangyaxuan@ruc.edu.cn>
2016-09-05 19:00:16 +0800

    test of popularRecommend

---
+++ library/policies/PopularRecommendPolicy.php
@@ -0,0 +1,151 @@
+<?php
+
+define ('RECOMMENT_DAY_SPAN', 3);
+define ('CLICK_DAY_SPAN', 3);
+
+class PopularRecommendPolicy extends BaseListPolicy {
+    public function __construct($di) {
+        parent::__construct($di);
+        $this->esClient = $di->get('elasticsearch');
+        $this->logger = $di->get('logger');
+    }
+
+    protected static function _getRecFromCache($sign) {
+        $cache = DI::getDefault()->get('cache');
+        if ($cache) {
+            $key = CACHE_NEWS_RECOMMEND_PREFIX . $sign;
+            $value = $cache->get($key);
+            if ($value) {
+                $recNewsIdLst = json_decode($value, true); 
+                if ($recNewsIdLst) {
+                    return $recNewsIdLst;
+                }
+            }
+        }
+        return array();
+    }
+
+    protected static function _saveRecToCache($newsSign, $recNewsObjLst){
+        $cache = DI::getDefault()->get('cache');
+        if ($cache) {
+            $key = CACHE_NEWS_RECOMMEND_PREFIX . $newsSign;
+            $cache->multi();
+            $newsSignLst = array();
+            foreach ($recNewsObjLst as $curSign) {
+                $newsSignLst[] = $curSign;
+            }
+
+            $cache->set($key, json_encode($newsSignLst, true));
+            $cache->expire($key, CACHE_NEWS_RECOMMEND_TTL);
+            $cache->exec();
+        }
+    }
+
+    protected function getRecommendNews($myself, $pn, $minThre=0.) {
+        $recNewsLst = self::_getRecFromCache($myself);
+        if ($recNewsLst) {
+            return $recNewsLst;
+        }
+
+        $searchParams = array(
+            'index' => 'banews-article',
+            'type' => 'article',
+            'body' => array(
+                'query' => array(
+                    'more_like_this' => array(
+                        'fields' => array('title', 'plain_text'),
+                        'like' => array(
+                            '_index' => 'banews-article',
+                            '_type' => 'article',
+                            '_id' => $myself,
+                        ),
+                        'max_query_terms' => 30,
+                        'min_term_freq' => 1,
+                        'min_doc_freq' => 1,
+                    ),
+                ),
+            ),
+        );
+        try {
+            $resLst = array();
+            $relatedNews = $this->esClient->search($searchParams);
+            if (array_key_exists('hits', $relatedNews)) {
+                if (array_key_exists('hits', $relatedNews['hits'])) {
+                    foreach($relatedNews['hits']['hits'] as $curNews) {
+                        if ($curNews['_score'] < $minThre) {
+                            continue;
+                        }
+
+                        $resLst[] = $curNews["_id"];
+                        if (count($resLst) > $pn)
+                            break;
+                    }
+                }
+            }
+
+            if ($resLst) {
+                self::_saveRecToCache($myself, $resLst);
+            }
+            return $resLst;
+        } catch(\Exception $e) {
+            #$this->logger->error(sprintf("[file:%s][line:%s][message:%s][code:%s]", 
+            #    $e->getFile(), $e->getLine(), $e->getMessage(), $e->getCode()));
+            return array();
+        }
+    }
+
+    public function sampling($channel_id, $device_id, $user_id, $pn, 
+        $day_till_now, $prefer, array $options = array()) {
+        $sentLst = $this->_cache->getDeviceSeen($device_id);
+        //TODO: get all user manipulations
+        #$clickedLst = $this->_cache->getDeviceClicked($device_id);
+        $temp = array_slice($sentLst, 0, 5);
+        $clickedLst = array('lD6ti4AUGEI%3D','TKfrXMyJF2c%3D', 'v6lj%2BiwJvgk%3D');
+        //TODO: filter fetch_time of clickedLst(do not know the field name)
+        #$filteredClickedLst = $this->timeFilter($clickedLst, 2);
+        
+        // recommended news for user <- clickedLst
+        $recommendLst = array();
+        foreach($clickedLst as $news_id) {
+            $resLst = $this->getRecommendNews($news_id, 5, 0);
+            array_push($recommendLst, $resLst); 
+        }
+
+        $filterTimeNewsLst = $this->timeFilter($recommendLst, RECOMMEND_DAY_SPAN);
+        $filterSentNewsLst = $this->sentFilter($sentLst, $filterTimeNewsLst);
+        if (!$filterSentNewsLst or (count($filterSentNewsLst) < $pn)) {
+            return array();
+        } else {
+            //random select news 
+            shuffle($filterSentNewsLst);
+            return array_slice($filterSentNewsLst, 0, $pn);        
+        }
+
+    }
+
+    protected function sentFilter($sentNewsLst, $newsLst) {
+        $filterNewsLst = array();
+        foreach ($newsLst as $news) {
+            if (!in_array($news, $sentNewsLst)) {
+                array_push($filterNewsLst, $news); 
+            }
+        }
+        return $filterNewsLst;
+    }
+
+    protected function timeFilter($newsLst, $timeSpan) {
+        $filterNewsLst = array();
+        $now = time();
+        $start = ($now - ($timeSpan * 86400));
+        $start = $start - ($start % 86400);
+        $end = ($now + 86400) - (($now + 86400) % 86400);
+
+        foreach ($newsLst as $news) {
+            if($news->fetch_time>=start and $news->fetch_time<=end){
+                array_push($filterNewsLst, $news); 
+            }
+        }
+        return filterNewsLst;
+    }
+
+}
--- library/selectors/Selector10001.php
+++ library/selectors/Selector10001.php
@@ -12,6 +12,7 @@
 define('MIN_NEWS_COUNT', 8);
 define('MAX_NEWS_COUNT', 10);
 define("LATELY_NEWS_COUNT", 2);
+define('RECOMMEND_NEWS_COUNT',2);
 
 class Selector10001 extends BaseNewsSelector{
 
@@ -32,13 +33,24 @@ class Selector10001 extends BaseNewsSelector{
     public function sampling($sample_count, $prefer) {
         $randomPolicy = new ExpDecayListPolicy($this->_di); 
         $popularPolicy = new PopularListPolicy($this->_di); 
+        $popularRecommendPolicy = new PopularRecommendPolicy($this->_di);
         $options = array();
         if ($prefer == "later") {
             $options["long_tail_weight"] = 0;
         }
-        $popularNewsCnt = max($sample_count - LATELY_NEWS_COUNT, 1);
+        $popularNewsCnt = max($sample_count - LATELY_NEWS_COUNT - RECOMMEND_NEWS_COUNT, 1);
         $popularNewsLst = $popularPolicy->sampling($this->_channel_id, $this->_device_id,
                 $this->_user_id, $popularNewsCnt, 3, $prefer, $options);
+        $recommendNewsLst = $popularRecommendPolicy->sampling($this->_channel_id, $this->_device_id,
+                $this->_user_id, RECOMMEND_NEWS_COUNT, 3, $prefer, $options);
+
+        foreach($recommendNewsLst as $recNews) {
+            if(in_array($recNews, $popularNewsLst)) {
+                continue;
+            }
+            $popularNewsLst[] = $recNews;
+        }
+
         $randomNewsLst = $randomPolicy->sampling($this->_channel_id, $this->_device_id,
                 $this->_user_id, MAX_NEWS_COUNT, 3, $prefer, $options);
 
