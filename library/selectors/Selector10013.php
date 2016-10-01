7ace4b3aece0f92b4c6f3ec3e9f7da31acb13018
zhangguanxing <zgxcassar@gmaol.com>
2016-10-01 15:55:40 +0800

    add all

--- library/selectors/Selector10013.php
+++ library/selectors/Selector10013.php
@@ -1,3 +1,12 @@
 <?php
 class Selector10013 extends Selector10004 {
+    public function select($prefer) {
+        $ret = parent::select($prefer);
+
+        if ($prefer == "later") {
+            $ret[0] = INTERVENE_TPL_CELL_PREFIX . NEWS_LIST_TPL_NBA;
+        }
+
+        return $ret;
+    }
 }
