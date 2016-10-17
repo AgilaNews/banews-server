26d0930c2647c5133b999ce1c132588a55cff990
zhangguanxing <zgxcassar@gmaol.com>
2016-10-17 16:38:16 +0800

    remove submodule

--- 2/app/controllers/UserController.php
+++ 2/app/controllers/UserController.php
@@ -196,60 +196,6 @@ class UserController extends BaseController {
         return $ret;
     }
 
-
-    public function LikeAction(){
-        if (!$this->request->isPost()){
-            throw new HttpException(ERR_INVALID_METHOD,
-                                    "add new comment error");
-        }
-
-        if (!$this->deviceId) {
-            throw new HttpException(ERR_DEVICE_NON_EXISTS, "device id not found");
-        }
-        
-        $param = $this->request->getJsonRawBody(true);
-        $comment_id = $this->get_or_fail($param, "comment_id", "int");
-        if ($this->userSign) {
-            // if set user sign, he must exists
-            // but client could ignoring this field
-            $user_model = User::getBySign($this->userSign);
-            if (!$user_model) {
-                throw new HttpException(ERR_USER_NON_EXISTS,
-                                        "user non exists");
-            }
-        }
-
-        $comment_service = $this->di->get('comment');
-
-        $req = new iface\LikeCommentRequest();
-        $req->setProduct($this->config->comment->product_key);        
-        $req->setCommentId($comment_id);
-        $req->setDeviceId($this->deviceId);
-        $req->setUserId($this->userSign);
-
-        list($resp, $status) = $comment_service->LikeComment($req)->wait();
-
-        if ($status->code != 0) {
-            throw new HttpException(ERR_INTERNAL_BG,
-                                    $status->details);
-        }
-        
-        $s = $resp->getResponse();
-        if ($s->getCode() != iface\GeneralResponse\ErrorCode::NO_ERROR) {
-            throw new HttpException(ERR_INTERNAL_BG,
-                                    $s->getErrorMsg()
-                                    );
-        }
-
-        $currnetLiked = $resp->getCurrentLiked();
-        $this->setJsonResponse(array(
-                                    "message" => "ok",
-                                    "liked" => $currnetLiked,
-                                    ));
-
-        return $this->response;
-    }
-
     private function batchCollect(){
         if (!$this->userSign) {
             throw new HttpException(ERR_NOT_AUTH, "userid not seted");
