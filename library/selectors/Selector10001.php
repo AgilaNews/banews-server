8b15d474f68b3d1dffb44fcecc7963d543533b23
zhangguanxing <zgxcassar@gmaol.com>
2016-10-21 14:21:55 +0800

    merge

--- library/selectors/Selector10001.php
+++ library/selectors/Selector10001.php
@@ -86,7 +86,6 @@ class Selector10001 extends BaseNewsSelector{
     public function select($prefer) {
         $required = mt_rand(MIN_NEWS_COUNT, MAX_NEWS_COUNT);
         $selected_news_list = $this->sampling($required, $prefer); 
-        $selected_news_list []= "MBfj0txvyfw=";
         $models = News::BatchGet($selected_news_list);
         $models = $this->removeInvisible($models);
         $models = $this->removeDup($models);
