880273558a6a5b112a93cd00f28f6ba539503ea8
zhangguanxing <zgxcassar@gmaol.com>
2016-10-27 18:28:02 +0800

    change thshold

--- library/selectors/Selector10013.php
+++ library/selectors/Selector10013.php
@@ -1,5 +1,11 @@
 <?php
+define('NBA_LATELY_NEWS_COUNT', 6);
+
 class Selector10013 extends Selector10004 {
+    public function getLatelyNewsCount(){
+        return NBA_LATELY_NEWS_COUNT;
+    }
+
     public function select($prefer) {
         $ret = parent::select($prefer);
 
