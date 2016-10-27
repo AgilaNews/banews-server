cc923188433a5604fc25d28372a7d92fabf48234
zhangguanxing <zgxcassar@gmaol.com>
2016-10-27 14:10:10 +0800

    add all

--- 2/app/controllers/NewsController.php
+++ 2/app/controllers/NewsController.php
@@ -263,7 +263,7 @@ class NewsController extends BaseController {
                
        $ow = $meta["width"];
        $oh = $meta["height"];
-       if ($this->_os == "ios") {
+       if ($this->os == "ios") {
            $aw = (int) ($this->_screen_w  - 44);
        } else {
            $aw = (int) ($this->_screen_w * 11 / 12);
