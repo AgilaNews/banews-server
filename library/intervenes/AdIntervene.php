eb573cd65d9f7f1da26a0e36d0ceead5f03dfd8e
zhangguanxing <zgxcassar@gmaol.com>
2016-11-01 11:32:56 +0800

    add config

---
+++ library/intervenes/AdIntervene.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file   AdIntervene.php
+ * @author Gethin Zhang <zgxcassar@gmail.com>
+ * @date   Mon Oct 31 21:40:47 2016
+ * 
+ * @brief  
+ * 
+ * 
+ */
+use Phalcon\DI;
+
+class AdIntervene extends BaseIntervene {
+    public function render(){
+        switch ($this->context["type"]) {
+        case "fb":
+            return $this->renderFB();
+            
+        default:
+            assert(false, "not implement ad type: " . $this->context["type"]);
+        }
+    }
+
+    protected function renderFB(){
+        $di = DI::getDefault();
+        $redis = $di->get('redis');
+        $device = $this->context["device"];
+        $key = sprintf(CACHE_AD_ID_KEY, "FB", $device);
+            
+        if ($redis) {
+            $v = $redis->incr($key);
+            $redis->expire($key, CACHE_AD_ID_TTL);
+        }
+
+        return array(
+                     "tpl" => NEWS_LIST_TPL_AD_FB_MEDIUM,
+                     "ad_id" => $v,
+                     );
+    }
+}
