87e114d76ed8869d887d6f80178e26e2eaccaa29
limeng <lemon.li.hust@gmail.com>
2016-11-23 16:59:01 +0800

    fix bug

--- library/selectors/Selector10001.php
+++ library/selectors/Selector10001.php
@@ -80,11 +80,9 @@ class Selector10001 extends BaseNewsSelector{
             $popularNewsLst = $popularPolicy->sampling($this->_channel_id, 
                 $this->_device_id, $this->_user_id, 50, 3, $prefer, 
                 $options);
-            $candidateNewsLst = array_unique(array_merge($popularNewsLst,
-                $topicRecNewsLst)); 
             $lrRanker = new LrNewsRanker($this->_di); 
             $recNewsLst = $lrRanker->ranking($this->_channel_id,
-                $this->_device_id, $candidateNewsLst, $prefer, $sample_count);
+                $this->_device_id, $popularNewsLst, $prefer, $sample_count);
         }
             
         if (count($recNewsLst) < $sample_count) {
