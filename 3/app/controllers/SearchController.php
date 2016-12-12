ddc78df23388982872e77e485bb01a029df49aa0
lantian <lanhero@gmail.com>
2016-12-12 21:39:31 +0800

    add search

--- 3/app/controllers/SearchController.php
+++ 3/app/controllers/SearchController.php
@@ -9,6 +9,13 @@
  * 
  */
 class SearchController extends BaseController {
+
+    public function __construct($di) {
+        parent::__construct($di);
+        $this->esClient = $di->get('elasticsearch');
+        $this->logger = $di->get('logger');
+    }
+
     public function HotwordsAction() {
         $this->setJsonResponse(array(
                                      "hotwords" => array("lian", "zhan", "animal"),
@@ -17,7 +24,32 @@ class SearchController extends BaseController {
     }
 
     public function IndexAction() {
-        
+        $channel_id = $this->get_request_param("channel_id", "int", true);
+        $from = $this->get_request_param("from", "int", true);
+        $size = $this->get_request_param("size", "int", true);
+        $words = $this->get_request_param("words", "string", true);
+
+        $searchParams = array(
+            'index' => 'banews-article',
+            'type'  => 'article',
+            'body'  => array(
+                'query' => array(
+                    'match' => array('title', $words),
+                ),
+                'highlight' => array(
+                    'fields' => array('content', array())
+                ),
+            ),
+        );
+        try {
+            $resLst = array();
+            $relatedNews = $this->esClient->search($searchParams);
+            return $resLst;
+        } catch(\Exception $e) {
+            #$this->logger->error(sprintf("[file:%s][line:%s][message:%s][code:%s]", 
+            #    $e->getFile(), $e->getLine(), $e->getMessage(), $e->getCode()));
+            return array();
+        }
     }
 }
 
