34db87a8000335afa5a54d2b153bbe649c0ea0db
Zhao Yulong <elysium.zyl@gmail.com>
2016-12-15 19:33:22 +0800

    *) add log

--- 2/app/controllers/TopicController.php
+++ 2/app/controllers/TopicController.php
@@ -9,7 +9,7 @@
 
 class TopicController extends BaseController {
     protected function formatTopic($topic) {
-        $meta = json_decode($topic, true);
+        $meta = json_decode($topic->image_meta, true);
         return array(
             "title" => $topic->title,
             "publish_time" => $topic->publish_time,
@@ -45,6 +45,12 @@ class TopicController extends BaseController {
                 $render = new RenderTopicNews($this);
                 $ret["news"] = $render->render($models);
             }
+            $this->logger->info(sprintf("[Detail][Topic:%s][news:%d]", $topic_id, count($news)));
+
+            $this->logEvent(EVENT_TOPIC_DETAIL, array(
+                                               "topic_id"=> $topic_id,
+                                               "news"=> $news,
+                                                ));
         }
         $this->setJsonResponse($ret);
         return $this->response;
