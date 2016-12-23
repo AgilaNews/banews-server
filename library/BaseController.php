3ca012e95855ffa72a235e8b85e3c64b7f363389
zhangguanxing <zgxcassar@gmaol.com>
2016-12-23 11:43:40 +0800

    changeabtest

--- library/BaseController.php
+++ library/BaseController.php
@@ -198,15 +198,16 @@ class BaseController extends Controller{
 
 
     private function initAbFlag() {
-        $ctx = new iface\RequestContext();
-        
+        $ctx = new ipeninsula\RequestContext();
+
+        $ctx->ProductId = $this->config->abtest->product_key;
         $ctx->UserId = $this->userSign;
         $ctx->DeviceId = $this->deviceId;
         $ctx->SessionId = $this->session;
         $ctx->UserAgent = $this->ua;
         $ctx->Net = $this->net;
         $ctx->Isp = $this->isp;
-        $ctx->Language = $this->lang;
+        //        $ctx->Language = $this->lang;
         $ctx->ClientVersion = $this->client_version;
         $ctx->Os = $this->os;
         $ctx->OsVersion = $this->os_version;
