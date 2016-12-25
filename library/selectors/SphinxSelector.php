e3f72368d8134cd10e754fd01bbd459edfd20cd4
zhangguanxing <zgxcassar@gmaol.com>
2016-12-25 22:26:41 +0800

    change sphinx

--- library/selectors/SphinxSelector.php
+++ library/selectors/SphinxSelector.php
@@ -11,20 +11,24 @@
 use Phalcon\DI;
 class SphinxSelector extends BaseNewsSelector {
     public function __construct($channel_id, $controller) {
-        $config = DI::getDefault()->get('config');
-
         $this->ctx = RequestContext::GetCtxFromController($controller);
-
         parent::__construct($channel_id, $controller);
     }
 
-    protected function sampling($sample_count, $prefer) {
+    protected function select($prefer) {
         $sphinx = DI::getDefault()->get('sphinx');
+        $required = mt_rand(MIN_NEWS_SEND_COUNT, MAX_NEWS_SENT_COUNT);
 
-        $selected_news_list = $sphinx->select($this->ctx, $this->_channel_id, $prefer, $sample_count);
+        $selected_news_list = $sphinx->select($this->ctx, $this->_channel_id, $prefer, $required);
         if ($selected_news_list === null) {
             //TODO
         }
+        
+        $models = News::BatchGet($selected_news_list);
+        $models = $this->removeInvisible($models);
+        $models = $this->removeDup($models);
+
+        $this->insertAd($selected_news_list);
 
         return $selected_news_list;
     }
