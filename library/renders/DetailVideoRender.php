ab2ed6caf88065511d3150c8b448660b905c2414
zhangguanxing <zgxcassar@gmaol.com>
2017-01-08 14:43:49 +0800

    debugush

--- library/renders/DetailVideoRender.php
+++ library/renders/DetailVideoRender.php
@@ -11,11 +11,31 @@
 class DetailVideoRender extends BaseDetailRender {
     public function render($news_model, $recommend_models = null) {
         $ret = parent::render($news_model, $recommend_models);
-        $video = Video::getByNewsSign($news_model->url_sign);
-        if ($video) {
-            $ret["views"] = $video->view;
-        }
 
+        $ret["videos"] = array();
+        $view = 0;
+        
+        $videos = NewsYoutubeVideo::getVideosOfNews($newsSign);
+        foreach($videos as $video) {
+            if (!$video || $video->is_deadlink == 1 || !$video->cover_meta) {
+                continue;
+            }
+            
+            if ($video->cover_origin_url) {
+                $cover_meta = json_decode($video->cover_meta, true);
+                if (!$cover_meta || !$cover_meta["width"] || !$cover_meta["height"]) {
+                    continue;
+                }
+            }
+            
+            $c = $this->getImgCell($video->video_url_sign, $cover_meta);
+            $c["video_pattern"] = $c["pattern"] . "|v=1";
+            $c["youtube_id"] = $video->youtube_video_id;
+            $c["name"] = "<!--YOUTUBE" . $video->news_pos_id . "-->";
+            $view = $video->view;
+            $ret["videos"] []= $c;
+        }
+        
         return $ret;
     }
 }
