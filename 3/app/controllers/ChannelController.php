5cfc078d2bbc3a4ada5ad8697111e395f6193526
zhangguanxing <zgxcassar@gmaol.com>
2017-01-23 23:35:12 +0800

    add all

--- 3/app/controllers/ChannelController.php
+++ 3/app/controllers/ChannelController.php
@@ -13,7 +13,6 @@ class ChannelController extends BaseController {
         $version = $this->get_request_param("version", "int", true);
 
         $channels = ChannelV2::getChannelsOfVersion($version, $this->client_version, $this->os);
-        
         $this->logger->info(sprintf("[CHANNEL_GET][version:%s][ret:%d]", $version, count($channels)));
         $this->setJsonResponse($channels);
         return $this->response;
