ec2a9df10ea35917e1cb29289092a52dc9b8468e
zhangguanxing <zgxcassar@gmaol.com>
2017-02-06 21:12:10 +0800

    refractor factory method

---
+++ library/renders/NbaListRender.php
@@ -0,0 +1,29 @@
+<?php
+class NbaListRender extends BaseListRender {
+    public function __construct($controller){
+        parent::__construct($controller);
+    }
+
+    public function render($models) {
+        $intervenes = array();
+        $normal = array();
+        
+        foreach ($models as $idx => $model) {
+            if ($model instanceof NbaIntervene) {
+                if (Features::Enabled(Features::FIX_NBA_TOP_FEATURE, $this->client_version, $this->os)) {
+                    $intervenes[$idx] = $model->render();                  
+                }
+            } else {
+                $normal []= $model;
+            }
+        }
+
+        $ret = parent::render($normal);
+
+        foreach ($intervenes as $idx => $intervene) {
+            array_splice($ret, $idx, 0, $intervene);
+        }
+        
+        return $ret;
+    }
+}
