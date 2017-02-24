e94f94078047d03f6211bcf634f62740573f9d77
Zhao Yulong <elysium.zyl@gmail.com>
2017-02-24 10:38:36 +0800

    *)

--- library/policies/PersonalVideoInterestPolicy.php
+++ library/policies/PersonalVideoInterestPolicy.php
@@ -87,11 +87,15 @@ class PersonalVideoInterestPolicy extends BaseListPolicy {
     private function getUserInterestVideos($device_id) {
         $userYoutubeInterests = $this->getUserYoutubeInterests($device_id);
         $userYoutubeChannels = $this->samplingUserYoutubeChannel($userYoutubeInterests);
-
-        $videos = Video::getVideosByChannels($userYoutubeChannels);
+        $tmp = array();
+        foreach ($userYoutubeChannels as $youtubeChannel) {
+            $videos = Video::getVideosByAuthor($youtubeChannel);
+            shuffle($videos);
+            $tmp = array_merge($tmp, array_slice($videos,0, MAX_VIDEO_ONE_CHANNEL));
+        }
         $ret = array();
-        foreach ($videos as $video) {
-            $ret[] = array("id" => $video->news_url_sign, "weight" => $video->update_time);
+        foreach ($tmp as $video_id) {
+            $ret[] = array("id" => $video_id, "weight"=>1);
         }
         return $ret;
     }
